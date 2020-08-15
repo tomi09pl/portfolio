f07edb94ed321524b18f0e96be405166a3fef95b
tomi09pl <tomi09pl@gmail.com>
2020-08-15 12:37:22 +0100

    Updates

---
+++ precache-manifest.9aa6ba794e5146bc4efb09cccd7b8ac8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a033eb01cc1af7bc031fd520679aa05",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bd019f422a2a2268c4bb",
+    "url": "/portfolio/static/css/2.164144ba.chunk.css"
+  },
+  {
+    "revision": "7a3ce71c4a682414af6b",
+    "url": "/portfolio/static/css/main.4ca87ac6.chunk.css"
+  },
+  {
+    "revision": "bd019f422a2a2268c4bb",
+    "url": "/portfolio/static/js/2.48e9d8d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.48e9d8d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3ce71c4a682414af6b",
+    "url": "/portfolio/static/js/main.97ad617b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0b4171edfddd59b4f81a3b719f07e59c",
+    "url": "/portfolio/static/media/billycune.0b4171ed.png"
+  },
+  {
+    "revision": "bdce22a9b1a7fdc6f1bcc765551e0149",
+    "url": "/portfolio/static/media/eyoga.bdce22a9.png"
+  },
+  {
+    "revision": "1a9d73a5bebfa2a99af6b1a1ba56caab",
+    "url": "/portfolio/static/media/giveaway.1a9d73a5.png"
+  },
+  {
+    "revision": "b6d770f36468205557a06f64b061ef7c",
+    "url": "/portfolio/static/media/portfolio.b6d770f3.png"
+  }
+]);
